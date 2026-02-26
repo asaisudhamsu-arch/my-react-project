3fd643dfd2fe9afd4d07a3a072e6fff5f4b53206
Veera <asaisudhamu@gmail.com>
2026-02-26 14:12:16 -0600

    Initial React project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>We are your watchdog 🚀</h1>
+      <p>Secure your place successfully!</p>
+
+      <div>
+        <button>Secure</button>
+        <button>Medium</button>
+        <button>Large</button>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
+
+
